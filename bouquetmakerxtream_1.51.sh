8f8d566c23453911f326ab2261458ee8a44c4ab8
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-04-21 18:07:30 +0200

    Add files via upload

---
+++ bouquetmakerxtream_1.51.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-bouquetmakerxtream_1.51.20250418_all.ipk "https://raw.githubusercontent.com/Ham-ahmed/214/refs/heads/main/enigma2-plugin-extensions-bouquetmakerxtream_1.51.20250418_all.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-bouquetmakerxtream_1.51.20250418_all.ipk
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "**********************************************************
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panelv6                    *"
+echo "*                Enigma2 restart is required            *"
+echo "*********************************************************"
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo "====================================================="
+############################################                                                                                                                  
+echo ". >>>>         RESTARING         <<<<"
+echo "*********************************************************"
+wait
+killall -9 enigma2
+exit 0
